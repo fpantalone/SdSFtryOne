e67902981bbc7c9d7eea63e0ad3a6e877e1adad7
fpantalone <f.pantalone@gmail.com>
2020-08-05 22:42:12 +0200

    add layout for home view

--- app/src/main/java/be/technifutur/devmob9/sdsftryone/tools/LodingAsynchTask.kt
+++ app/src/main/java/be/technifutur/devmob9/sdsftryone/tools/LodingAsynchTask.kt
@@ -6,6 +6,6 @@ import be.technifutur.devmob9.sdsftryone.webservice.WebService
 class LodingAsynchTask(val listener: AsynchTaskListener): AsyncTask<Int, Void,AllTableResult> () {
     override fun doInBackground(vararg params: Int?): AllTableResult {
 
-        return WebService.updateDataBase()
+       return WebService.updateDataBase()
     }
 }
